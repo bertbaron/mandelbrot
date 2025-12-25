28b35638a07c3ec060b26e41335b11c5bf0bd178
Bert Baron <bertusbaron@gmail.com>
2025-12-25 14:16:30 +0100

    wplace palette

--- palette.js
+++ palette.js
@@ -203,12 +203,134 @@ const JEWELLERY = new IndexedPalette("jewellery", "Jewellery", [
     [0, 0, 153]
 ], false)
 
+// WPlace colors:
+const _WPLACE_PURPLE_HTML = [
+    // "#ffffff",
+    // "#ffffff",
+    "#ffffff",
+    "#b3b9d1",
+    "#6d758d",
+    "#333941",
+    "#4a4284",
+    "#7a71c4",
+    "#b5aef1",
+]
+
+const _CHARCOAL_PAINTING = [
+    "#F3EFE8",
+    "#D7D7D6",
+    "#BEBFC0",
+    "#7A7D80",
+    "#3B4348",
+    "#2E2E2E",
+    "#0B0B0B",
+    "#6B4F3A",
+]
+
+const _HOT_CHARCOAL = [
+    "#1A1A1A",
+    "#3C3C3C",
+    "#7A0C0C",
+    "#C02000",
+    "#FF4500",
+    "#FF8000",
+    "#FFC300",
+    "#FFFFCC",
+]
+const _WPLACE_RED_HTML = [
+    "#600018",
+    "#a50e1e",
+    "#ed1c24",
+    "#fa8072",
+    "#e45c1a",
+    "#ff7f27",
+    "#f6aa09",
+    "#f9dd3b",
+    "#fffabc",
+]
+
+const _WPLACE_ALL_HTML = [
+    "#000000",
+    "#3c3c3c",
+    "#787878",
+    "#aaaaaa",
+    "#d2d2d2",
+    "#ffffff",
+    "#600018",
+    "#a50e1e",
+    "#ed1c24",
+    "#fa8072",
+    "#e45c1a",
+    "#ff7f27",
+    "#f6aa09",
+    "#f9dd3b",
+    "#fffabc",
+    "#9c8431",
+    "#c5ad31",
+    "#e8d45f",
+    "#4a6b3a",
+    "#5a944a",
+    "#84c573",
+    "#0eb968",
+    "#13e67b",
+    "#87ff5e",
+    "#0c816e",
+    "#10aea6",
+    "#13e1be",
+    "#0f799f",
+    "#60f7f2",
+    "#bbfaf2",
+    "#28509e",
+    "#4093e4",
+    "#7dc7ff",
+    "#4d31b8",
+    "#6b50f6",
+    "#99b1fb",
+    "#4a4284",
+    "#7a71c4",
+    "#b5aef1",
+    "#780c99",
+    "#aa38b9",
+    "#e09ff9",
+    "#cb007a",
+    "#ec1f80",
+    "#f38da9",
+    "#9b5249",
+    "#d18078",
+    "#fab6a4",
+    "#684634",
+    "#95682a",
+    "#dba463",
+    "#7b6352",
+    "#9c846b",
+    "#d6b594",
+    "#d18051",
+    "#f8b277",
+    "#ffc5a5",
+    "#6d643f",
+    "#948c6b",
+    "#cdc59e",
+    "#333941",
+    "#6d758d",
+    "#b3b9d1",
+]
+function hexToRgb(hex) {
+    const bigint = parseInt(hex.slice(1), 16);
+    const r = (bigint >> 16) & 255;
+    const g = (bigint >> 8) & 255;
+    const b = bigint & 255;
+    return [r, g, b]
+}
+
+const WPLACE = new IndexedPalette("wplace", "WPlace", _WPLACE_PURPLE_HTML.map(hexToRgb), false, false)
+
 export const PALETTES = [
     ORIGINAL,
     MANDELBROT,
     LAVA,
     FALL,
     OCEAN,
+    WPLACE,
     SKY_WATER,
     POP,
     JEWELLERY,
